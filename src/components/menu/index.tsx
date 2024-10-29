2322565425b1e3a2574050e55b770013ee4d8177
VladRaier <barushkoll@gmail.com>
2024-10-29 23:30:24 +0300

    update

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,21 +1,29 @@
-import styles from "./menu.module.scss";
+import styles from './menu.module.scss'
+
+const item: { id: number; value: string }[] = [
+	{ id: 1, value: 'Об университете' },
+	{ id: 2, value: 'Приёмная компания' },
+	{ id: 3, value: 'Образование' },
+	{ id: 4, value: 'Наука' },
+	{ id: 5, value: 'Международная деятельность' },
+	{ id: 6, value: 'Дополнительное образование и профессиональное обучение' },
+	{ id: 7, value: 'Культура и спорт' },
+	{ id: 8, value: 'Пресс служба' },
+	{ id: 9, value: 'Контакты Вакансии' },
+]
 
 const Menu = () => {
-  return (
-    <ul className={styles.menu}>
-      <li className={styles.menu__item}>Об университете</li>
-      <li className={styles.menu__item}>Приёмная компания</li>
-      <li className={styles.menu__item}>Образование</li>
-      <li className={styles.menu__item}>Наука</li>
-      <li className={styles.menu__item}>Международная деятельность</li>
-      <li className={styles.menu__item}>
-        Дополнительное образование и профессиональное обучение
-      </li>
-      <li className={styles.menu__item}>Культура и спорт</li>
-      <li className={styles.menu__item}>Пресс служба</li>
-      <li className={styles.menu__item}>Контакты Вакансии</li>
-    </ul>
-  );
-};
+	return (
+		<ul className={styles.menu}>
+			{item?.map(item => {
+				return (
+					<li key={item.id} className={styles.menu__item}>
+						<span>{item.value}</span>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
 
-export default Menu;
+export default Menu
